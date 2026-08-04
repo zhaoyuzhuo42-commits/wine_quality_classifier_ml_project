f2d50c59b25f9fb47e477c1608729d5e802de0e3
zhaoyu <zhaoyuzhuo42@gmail.com>
2026-08-04 22:22:17 +0800

    feat: add project configuration

---
+++ src/config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+DATA_DIR =PROJECT_ROOT/"data"
+RAW_DATA_DIR = PROJECT_ROOT/"data"/"raw"
+PROCESSED_DATA_DIR = PROJECT_ROOT/"data"/"processed"
